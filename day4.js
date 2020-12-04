2b3934b63de792b29f5146606b14b3fba923da63
Ken Johnson <kenj0418@gmail.com>
2020-12-03 23:39:42 -0600

    Day 4 - cleaned up

--- day4.js
+++ day4.js
@@ -1,27 +1,28 @@
 const {readStringArrayFromFile} = require("./lib");
 
-const parseLine = (line, currPassport) => {
+const parseLine = (line) => {
+  let currPassport = {}
   const entries = line.split(" ");
   entries.forEach((entry) => {
     const parts = entry.split(":");
     currPassport[parts[0]] = parts[1];
   })
+
+  return currPassport;
 }
 
-const parsePassports = (data) => {
-  let line = 0;
+const parsePassports = (lines) => {
   let passports = [];
   let currPassport = {}
-  while (line < data.length) {
-    const lineSt = data[line];
-    if (lineSt.length > 0) {
-      parseLine(lineSt, currPassport);
+  lines.forEach(line => {
+    if (line.length > 0) {
+      const newData = parseLine(line);
+      currPassport = {...currPassport, ...newData}
     } else {
       passports.push(currPassport);
       currPassport = {};
     }
-    line++;
-  }
+  })
 
   return passports;
 }
@@ -29,14 +30,14 @@ const parsePassports = (data) => {
 const isValid = (passport) => {
   /*
   byr (Birth Year)
-iyr (Issue Year)
-eyr (Expiration Year)
-hgt (Height)
-hcl (Hair Color)
-ecl (Eye Color)
-pid (Passport ID)
-OK: cid (Country ID)
-*/
+  iyr (Issue Year)
+  eyr (Expiration Year)
+  hgt (Height)
+  hcl (Hair Color)
+  ecl (Eye Color)
+  pid (Passport ID)
+  OK: cid (Country ID)
+  */
   return passport.byr && passport.iyr && passport.eyr && passport.hgt &&passport.hcl && passport.ecl && passport.pid;
 }
 
@@ -45,15 +46,15 @@ const isValid2 = (passport) => {
 
   /*
   byr (Birth Year) - four digits; at least 1920 and at most 2002.
-iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-hgt (Height) - a number followed by either cm or in:
-If cm, the number must be at least 150 and at most 193.
-If in, the number must be at least 59 and at most 76.
-hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-pid (Passport ID) - a nine-digit number, including leading zeroes.
-cid (Country ID) - ignored, missing or not.
+  iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  hgt (Height) - a number followed by either cm or in:
+  If cm, the number must be at least 150 and at most 193.
+  If in, the number must be at least 59 and at most 76.
+  hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  pid (Passport ID) - a nine-digit number, including leading zeroes.
+  cid (Country ID) - ignored, missing or not.
   */
 
   const by = parseInt(passport.byr);
@@ -66,24 +67,22 @@ cid (Country ID) - ignored, missing or not.
   if (passport.eyr.length != 4 || ey < 2020 || ey > 2030) return false;
 
   //hgt
-  if (passport.hgt.match(/[0123456789]+in/)) {
-    const inches = parseInt(passport.hgt.split("in"));
-    if (inches < 59 || inches > 76) return false;
-  } else if (passport.hgt.match(/[0123456789]+cm/)) {
-    const cm = parseInt(passport.hgt.split("cm"));
-    if (cm < 150 || cm > 193) return false;
-  } else {
-    return false;
-  }
+  const hgParts = passport.hgt.match(/^([0-9]+)((?:cm|in))$/)
+  if (!hgParts) return false;
+  const hgUnit = hgParts[2];
+  const hg = hgParts[1];
+
+  if (hgUnit == "in" && (hg < 59 | hg > 76)) return false;
+  if (hgUnit == "cm" && (hg < 150 | hg > 193)) return false;
 
   //hcl
-  if (passport.hcl.length != 7 || !passport.hcl.match(/\#[0123456789abcdef]+/)) return false;
+  if (!passport.hcl.match(/^\#[0-9a-f]{6}$/)) return false;
 
   //ecl
   if (["amb","blu","brn","gry","grn","hzl","oth"].indexOf(passport.ecl) < 0) return false;
 
-  const pid = parseInt(`1${passport.pid}`);
-  return pid >= 1000000000 && pid <= 1999999999;
+  //pid
+  return passport.pid.match(/^[0-9]{9}$/);
 }
 
 
