04041ce60c065a0668b6f4976dd44e4157e1f317
Ken Johnson <kenj0418@gmail.com>
2020-12-04 23:21:13 -0600

    Day 5 - slight cleanups

--- day5.js
+++ day5.js
@@ -1,25 +1,5 @@
 const {readStringArrayFromFile} = require("./lib");
 
-const max = (arr) => {
-  let maxSoFar = 0;
-  arr.forEach((val) => {
-    if (val > maxSoFar) {
-      maxSoFar = val;
-    }
-  });
-  return maxSoFar;
-}
-
-const min = (arr) => {
-  let minSoFar = 999999999;
-  arr.forEach((val) => {
-    if (val < minSoFar) {
-      minSoFar = val;
-    }
-  });
-  return minSoFar;
-}
-
 const parseSeat = (line) => {
   const row = parseInt(line.slice(0, 7).replace(/F/g, "0").replace(/B/g, "1"),2);
   const col = parseInt(line.slice(7).replace(/L/g, "0").replace(/R/g, "1"),2);
@@ -31,10 +11,9 @@ const getSeatId = (seat) => {
   return seat.row * 8 + seat.col;
 }
 
-const findMissingSeat = (seats) => {
-  const seatIds = seats.map(getSeatId);
-  const minSeat = min(seatIds);
-  const maxSeat = max(seatIds);
+const findMissingSeat = (seatIds) => {
+  const minSeat = Math.min(...seatIds);
+  const maxSeat = Math.max(...seatIds);
 
   for (let i = minSeat; i <= maxSeat; i++) {
     if (seatIds.indexOf(i) < 0) {
@@ -44,13 +23,11 @@ const findMissingSeat = (seats) => {
 }
 
 const run = () => {
-  const st = readStringArrayFromFile("./input/day5.txt", "\n");
-  const seats = st.map(parseSeat);
+  const seats = readStringArrayFromFile("./input/day5.txt", "\n").map(parseSeat);
   const seatIds = seats.map(getSeatId);
-  // console.log(seats);
-  // console.log(seatIds);
-  console.log(max(seatIds));
-  console.log(findMissingSeat(seats));
+
+  console.log(`Max Seat id: ${Math.max(...seatIds)}`);
+  console.log(`Missing Seat id: ${findMissingSeat(seatIds)}`);
 }
 
 module.exports = { run };
