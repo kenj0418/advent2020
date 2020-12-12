3347d0ec71fcd6497edc01b2d126de3217acb069
Ken Johnson <kenj0418@gmail.com>
2020-12-12 00:18:09 -0600

    Day 12 - Part 2 - initial

--- day12.js
+++ day12.js
@@ -2,23 +2,40 @@ const {readStringArrayFromFile} = require("./lib");
 
 const EAST = {x:1, y:0}
 const WEST = {x:-1, y:0}
-const NORTH ={x:0, y:-1}
-const SOUTH = {x:0, y:1}
-const dirs = [NORTH, WEST, SOUTH, EAST];
+const NORTH ={x:0, y:1}
+const SOUTH = {x:0, y:-1}
 
 const taxiDist = (ship) => {
   return Math.abs(ship.x) + Math.abs(ship.y);
 }
 
-const move = (ship, dir, qty) => {
-  ship.x += dir.x * qty;
-  ship.y += dir.y * qty;
+const moveWay = (ship, dir, qty) => {
+  ship.way.x += dir.x * qty;
+  ship.way.y += dir.y * qty;
 }
 
-const rot = (ship, deg) => {
-  const degIndex = Math.floor(deg / 90);
-  ship.dirIndex = (ship.dirIndex + degIndex) % 4;
-  ship.dir = dirs[ship.dirIndex];  
+const rotWay = (ship, deg) => {
+  const tempX = ship.way.x;
+  const tempY = ship.way.y;
+  if (deg == 0 || deg == 360) {
+    // nothing
+  } else if (deg == 180 || deg == -180) {
+    ship.way.x = -tempX;
+    ship.way.y = -tempY;
+  } else if (deg == 90 || deg == -270) {
+    ship.way.x = -tempY;
+    ship.way.y = tempX;
+  } else if (deg == 270 || deg == -90) {
+    ship.way.x = tempY;
+    ship.way.y = -tempX;
+  } else {
+    throw new Error(deg)
+  }
+}
+
+const forward = (ship, qty) => {
+  ship.x += ship.way.x * qty;
+  ship.y += ship.way.y * qty;
 }
 
 
@@ -27,15 +44,16 @@ const moveForEach = (ship, instr) => {
   const qty = parseInt(instr.slice(1));
 
   switch (action) {
-    case "N": move(ship, NORTH, qty); break;
-    case "S": move(ship, SOUTH, qty); break;
-    case "W": move(ship, WEST, qty); break;
-    case "E": move(ship, EAST, qty); break;
-    case "F": move(ship, ship.dir, qty); break;
-    case "L": rot(ship, qty); break;
-    case "R": rot(ship, 360 - qty); break;
+    case "N": moveWay(ship, NORTH, qty); break;
+    case "S": moveWay(ship, SOUTH, qty); break;
+    case "W": moveWay(ship, WEST, qty); break;
+    case "E": moveWay(ship, EAST, qty); break;
+    case "F": forward(ship, qty); break;
+    case "L": rotWay(ship, qty); break;
+    case "R": rotWay(ship, -qty); break;
     default: throw new Error(action);
   }
+  console.log(instr, ship);
 }
 
 const moveForInstructions = (ship, instructions) => {
@@ -46,17 +64,20 @@ const run = () => {
   let st = readStringArrayFromFile("./input/day12.txt", "\n");
   
   let ship = {
-    dir: EAST,
-    dirIndex: 3,
     x: 0,
-    y: 0
+    y: 0,
+    way: {
+      x: 10,
+      y: 1
+    }
   }
 
   moveForInstructions(ship, st);
 
-  console.log(`Day 12 part 1:  ${taxiDist(ship)}`);
-
-  // console.log(`Day 12 part 2:  ${0}`);
+  console.log(`${taxiDist(ship)}`);
+  // not 79740
+  // not 79658
+  // not 58751
 }
 
 module.exports = { run };
