471f5b125eb9abfccae38a4fa837f041d0f905db
Ken Johnson <kenj0418@gmail.com>
2020-12-18 00:16:05 -0600

    Day 18 - cleanup

--- day18.js
+++ day18.js
@@ -1,66 +1,21 @@
 const {readStringArrayFromFile, sum} = require("./lib");
 
-const processParens = (tokens) => {
-  // console.log(`Process Parens ${tokens}`);
-  let closeParenCount = 1;
-  let pos = tokens.length - 2;
-  while (closeParenCount && pos >= 0) {
-    if (tokens[pos] == "(") {
-      closeParenCount--;
-      // console.log("OPEN");
-    } else if (tokens[pos] == ")") {
-      closeParenCount++;
-      // console.log("CLOSE");
-    }
-    pos--;
-  }
-
-  if (closeParenCount) {
-    throw new Error("Open Paren " + JSON.stringify(tokens));
-  }
-
-  const parenPart = evaluateTokens(tokens.slice(pos + 2, tokens.length - 1));
-  const returnArray = [...tokens.slice(0, pos + 1), parenPart];
-  // console.log(`Return Array: ${returnArray}`);
-  return returnArray;
+const simpleOrderOfOperationsNextOperator = (tokens) => {
+  return tokens.length - 2;
 }
 
-const evaluateTokens = (origTokens) => {
-  // console.log(`eval ${origTokens}`);
-  let tokens = origTokens;
-  if (tokens.length == 1) {
-    return parseInt(tokens[0]);
-  } else if (!tokens.length) {
-    console.log(`Tokens: ${tokens}`);
-    throw new Error("parsing error");
+const addSubFirstOrderOfOperationsNextOperator = (tokens) => {
+  const lastMul = tokens.lastIndexOf("*");
+  const lastDiv = tokens.lastIndexOf("/");
+  if (lastMul < 0 && lastDiv < 0) {
+    return tokens.length - 2;
+  } else if (lastMul < 0) {
+    return lastDiv;
+  } else if (lastMul > lastDiv) {
+    return lastMul;
+  } else {
+    return lastDiv;
   }
-
-  if (tokens[tokens.length - 1] == ")") {
-    tokens = processParens(tokens);
-    if (tokens.length == 1) {
-      return parseInt(tokens[0]);
-    }
-  }
-
-  const lastOperatorIndex = tokens.length - 2; // since already handling parens
-  const lastOperator = tokens[lastOperatorIndex];
-  const lhs = evaluateTokens(tokens.slice(0, lastOperatorIndex));
-  const rhs = evaluateTokens(tokens.slice(lastOperatorIndex + 1));
-
-  switch (lastOperator) {
-    case '+': return lhs + rhs;
-    case '-': return lhs - rhs;
-    case '/': return lhs / rhs;
-    case '*': return lhs * rhs;
-    default:
-      console.log(`Unknown operator: ${tokens}`);
-      throw new Error("Unknown operator: " + firstOperator);
-  }
-}
-
-const evaluate = (st) => {
-  const tokens = st.replace(/\(/g, "( ").replace(/\)/g, " )").split(" ");
-  return evaluateTokens(tokens);
 }
 
 const getEndParenPos = (tokens) => {
@@ -83,13 +38,13 @@ const getEndParenPos = (tokens) => {
   return pos - 1;
 }
 
-const procesAllParens2 = (tokens) => {
+const procesAllParens = (tokens, orderOfOperations) => {
   let newTokens = []
   let pos = 0;
   while (pos < tokens.length) {
     if (tokens[pos] == "(") {
       const endPos = pos + getEndParenPos(tokens.slice(pos));
-      const parenValue = evaluateTokens2(tokens.slice(pos + 1, endPos));
+      const parenValue = evaluateTokens(tokens.slice(pos + 1, endPos), orderOfOperations);
       newTokens.push(parenValue)
       pos = endPos + 1;
     } else {
@@ -101,23 +56,8 @@ const procesAllParens2 = (tokens) => {
   return newTokens;
 }
 
-const getLastMulDiv = (tokens) => {
-  const lastMul = tokens.lastIndexOf("*");
-  const lastDiv = tokens.lastIndexOf("/");
-  if (lastMul < 0) {
-    return lastDiv;
-  } else if (lastDiv < 0) {
-    return lastMul;
-  } else if (lastMul > lastDiv) {
-    return lastMul;
-  } else {
-    return lastDiv;
-  }
-}
-
-const evaluateTokens2 = (origTokens) => {
-  console.log(`eval ${origTokens}`);
-  const tokens = procesAllParens2(origTokens)
+const evaluateTokens = (origTokens, orderOfOperations) => {
+  const tokens = procesAllParens(origTokens, orderOfOperations)
   if (tokens.length == 1) {
     return parseInt(tokens[0]);
   } else if (!tokens.length) {
@@ -125,41 +65,36 @@ const evaluateTokens2 = (origTokens) => {
     throw new Error("parsing error");
   }
 
-  const mulDivPos = getLastMulDiv(tokens);
-  const operatorIndex = (mulDivPos >= 0) ? mulDivPos : tokens.length - 2;
+  const operatorIndex = orderOfOperations(tokens);
 
   const operator = tokens[operatorIndex];
-  console.log(operator);
-  console.log(`lhs: ${tokens.slice(0, operatorIndex)}`);
-  const lhs = evaluateTokens2(tokens.slice(0, operatorIndex));
-  console.log(`rhs: ${tokens.slice(operatorIndex + 1)}`);
-  const rhs = evaluateTokens2(tokens.slice(operatorIndex + 1));
+  const lhs = evaluateTokens(tokens.slice(0, operatorIndex), orderOfOperations);
+  const rhs = evaluateTokens(tokens.slice(operatorIndex + 1), orderOfOperations);
 
   switch (operator) {
-    case '+': console.log(`returns: ${lhs + rhs}`); return lhs + rhs;
-    case '-': console.log(`returns: ${lhs - rhs}`); return lhs - rhs;
-    case '/': console.log(`returns: ${lhs / rhs}`); return lhs / rhs;
-    case '*': console.log(`returns: ${lhs * rhs}`); return lhs * rhs;
+    case '+': return lhs + rhs;
+    case '-': return lhs - rhs;
+    case '/': return lhs / rhs;
+    case '*': return lhs * rhs;
     default:
       console.log(`Unknown operator: ${tokens}`);
       throw new Error("Unknown operator: " + operator);
   }
 }
 
-const evaluate2 = (st) => {
+const evaluate = (st, orderOfOperations) => {
   const tokens = st.replace(/\(/g, "( ").replace(/\)/g, " )").split(" ");
-  return evaluateTokens2(tokens);
+  return evaluateTokens(tokens, orderOfOperations);
 }
 
 const run = () => {
   let st = readStringArrayFromFile("./input/day18.txt", "\n");
 
-  const values = st.map(evaluate);
+  const values = st.map((expr) => {return evaluate(expr, simpleOrderOfOperationsNextOperator)});
   const answer = sum(values);
   console.log("ANSWER (Part 1):", answer);
 
-  const values2 = st.map(evaluate2);
-  // console.log(values2);
+  const values2 = st.map((expr) => {return evaluate(expr, addSubFirstOrderOfOperationsNextOperator)});
   const answer2 = sum(values2);
   console.log("ANSWER (Part 2):", answer2);
 }
