456ba9073133f21b4e2dd7c126263ae5d68091b2
Ken Johnson <kenj0418@gmail.com>
2020-12-18 23:48:27 -0600

    Day 19 - Part 1

---
+++ day19.js
@@ -0,0 +1,119 @@
+const {readStringArrayFromFile} = require("./lib");
+
+let cache = {}
+
+//todo could precompute possible lengths for rules
+
+const isMatchOptionRaw = (rules, option, msg) => {
+  // console.log(`Checking ${msg} matches ${option}`);
+  if (option.length == 0 && msg.length != 0) return false;
+  if (option.length > msg.length) return false;
+  if (option.length == 0 && msg.length == 0) return true;
+
+  for (let len = 1; len <= msg.length; len++) {
+    const startMsg = msg.slice(0, len);
+    const endMsg = msg.slice(len);
+    const restOfOption = option.slice(1);
+
+    if (isMatch(rules, option[0], startMsg) && isMatchOption(rules, restOfOption, endMsg)) {
+      // console.log(`Checking ${msg} matches ${option}: TRUE`);
+      return true;
+    }
+  }
+
+  // console.log(`Checking ${msg} matches ${option}: FALSE`);
+  return false;
+}
+
+const isMatchOption = (rules, option, msg) => {
+  const key = `${JSON.stringify(option)}:${msg}`;
+  let cacheValue = cache[key];
+  if (cacheValue == undefined) {
+    cacheValue = isMatchOptionRaw(rules, option, msg);
+    cache[key] = cacheValue;
+  }
+
+  return cacheValue;
+}
+
+const isMatch = (rules, ruleNum, msg) => {
+  const currRule = rules[ruleNum];
+  // console.log(`CHECKING ${JSON.stringify(currRule)} against ${msg}`);
+  if (currRule.id != ruleNum) {throw new Error("mismatch: " + ruleNum);}
+
+  if (currRule.literal) {
+    return currRule.literal == msg;
+  }
+
+  for (let optionNum = 0; optionNum < currRule.options.length; optionNum++) {
+    const currOption = currRule.options[optionNum];
+    if (isMatchOption(rules, currOption, msg)) {
+      // console.log(`SUCCESS ${JSON.stringify(currRule)} against ${msg}`);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+const parseOption = (optionSt) => {
+  return optionSt.split(" ").map(Number);
+}
+
+const parseRule = (ruleSt) => {
+  const literalMatch = ruleSt.match(/([0-9]*): \"(.*)\"/);
+  if (literalMatch) {
+    return {
+      id: parseInt(literalMatch[1]),
+      literal: literalMatch[2],
+    }
+  }
+
+  const optionMatch = ruleSt.match(/([0-9]*): (.*)/);
+  if (!optionMatch) {
+    throw new Error("parse error: " + ruleSt);
+  }
+
+  const id = parseInt(optionMatch[1]);
+  const optionSt = optionMatch[2];
+  const options = optionSt.split(" | ").map(parseOption);
+  return { id, options }
+}
+
+const parseRules = (ruleStArr) => {
+  let rules = new Array(ruleStArr.length);
+
+  ruleStArr.forEach(ruleSt => {
+    const rule = parseRule(ruleSt);
+    rules[rule.id] = rule;
+  });
+
+  return rules;
+}
+
+const run = () => {
+  let st = readStringArrayFromFile("./input/day19.txt", "\n\n");
+  let ruleSt = st[0].split("\n");
+  let rules = parseRules(ruleSt)
+  let msgSt = st[1].split("\n");
+
+  const matchingMsgs = msgSt.filter((msg) => {
+    const result = isMatch(rules, 0, msg);
+    console.log(`${result} ${msg}`);
+    return result;
+  })
+  console.log("ANSWER (Part 1):", matchingMsgs.length);
+
+  cache = {};
+  
+  // 8: 42 | 42 8
+  // 11: 42 31 | 42 11 31
+  // const matchingMsgs = msgSt.filter((msg) => {
+  //   const result = isMatch(rules, 0, msg);
+  //   console.log(`${result} ${msg}`);
+  //   return result;
+  // })
+  // console.log("ANSWER (Part 1):", matchingMsgs2.length);
+}
+
+module.exports = { run };
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const day = require("./day18");
+const day = require("./day19");
 
 day.run();
